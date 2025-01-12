a7f4cd8715116ce20aa7481d8b1031777548ffd7
Rahul Choudhary <rahulcloudeqs@gmail.com>
2025-01-12 09:58:55 +0530

    DataBase Connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
 const app = express();
-
-
-app.listen(3000, ()=>{
-    console.log("Port is Listening at Port 3000 !!")
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then( ()=>{
+  console.log("Database is Successfully connected")
 })
+.catch((err)=>{
+console.log(err)
+})
+app.listen(3000, () => {
+  console.log("Port is Listening at Port 3000 !!");
+});
